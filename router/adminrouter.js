5c83ee038b3bc2c7adec25a49b4437e1def3aff4
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-22 11:25:45 +0530

    cart edit error

--- router/adminrouter.js
+++ router/adminrouter.js
@@ -7,7 +7,6 @@ var bdy=require('body-parser')
 var mult=require('multer')
 var storage=mult.diskStorage({destination:function(req,file,ks){
 ks(null,path.join(__dirname,"/../uploads"))
-
 },
 filename:function(req,file,ks){
 ks(null,"image_"+req.body.iname+".jpg")
@@ -16,7 +15,8 @@ var up=mult({storage:storage})
 //var type=up.single('file1')
 rot.use(bdy.urlencoded({extended:true}))
 var mon=require('mongoose')
-var url="mongodb://localhost/dbadditem"
+var url="mongodb+srv://ashly:ashly@cluster0-nybb2.mongodb.net/winedb?retryWrites=true&w=majority"
+   // var url="mongodb://localhost/dbadditem"     
 var add=require("../model/additem")
 mon.connect(url,function(err){
     if(err)throw err
@@ -25,18 +25,33 @@ mon.connect(url,function(err){
     }
     
 })
+rot.get("/view",function(req,res){
+
+
+    add.find({},function(err,result){
+        if(err)throw err
+        else
+        {
+            console.log(result)
+            res.render("adminview",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}],arr_prd:result})
+        }
+    })
+    
+    })
+    rot.get("/viewpic/:pic",function(req,res){
+        
+        res.sendFile(path.join(__dirname+"/../uploads/"+req.params.pic));
+    })
+    
 rot.post("/check",function(req,res){
-    //var u="";
+   
     u=req.body.uname1;
-    //var u=Array();
-    //u=req.body.uname1;
     console.log(req.body.uname1)
     p=req.body.pass1;
-//    for(var i=0;i<u.length;i++){
     if(u=="Admin"){
       res.render("check",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
     }
-// }
+})
        // res.send("user...")
 rot.post("/add",up.single('file1'),function(req,res){
     console.log(req.body.iname)
@@ -45,20 +60,40 @@ rot.post("/add",up.single('file1'),function(req,res){
    e1.itemprice=req.body.iprice
    e1.itemqty=req.body.iqty 
    e1. itempic="image_"+req.body.iname+".jpg"
-    // res.post("/upload",type,function(req,res){
-    //     res.send("file uploaded ")
-    // })
     e1.save(function(err){
     if(err)
     throw err
 
 else{
-    res.render("adminview",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
+    res.redirect("/admin/view")
 }
 })  
-  //  res.send("file uploaded ")
-})
-rot.post("\view")
-
   
+})
+rot.get("/edit/:name",function(req,res){
+    name=req.params.name
+    add.findOne({itemname:name},function(err,result){
+        console.log(result)
+        res.render("prdedit",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}],arr_prd:result})
+    })
+})
+rot.get("/update/:name",function(req,res){
+    name=req.params.name
+    qty=req.body.iqty
+    console.log(qty)
+
+    price=req.body.iprice
+    console.log(price)
+    add.updateOne({itemname:name},{$set:{itemprice:price,itemqty:qty}},function(err,result){
+        console.log(result)
+        if (err) throw err;
+   
+ 
+        else{
+         console.log("1 document updated");
+        
+     res.redirect('/admin/view')
+        }
+    })
+    
 })
